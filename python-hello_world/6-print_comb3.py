0563301251c02c6e632ff0f11470b1bf503626bc
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-18 11:54:22 +0300

    change code

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,3 @@
-for i in range(1, 9):
-    for j in range(i + 1, 10):
-        print("{:02d}, ".format(i), end="")
-        print("{:02d}, ".format(j), end="")
-print("89")
-
+for i in range(10):
+    for j in range(i+1, 10):
+        print(f"{i}{j}", end=", " if i != 8 or j != 9 else "\n")
