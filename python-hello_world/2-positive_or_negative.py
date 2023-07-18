5b5ac3656584e757e3d13f06f729f7d739e73323
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-18 06:59:30 +0300

    add 2-positive_or_ negative.py

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+
+def random_sign():
+
+
+    if number > 0:
+        sign = "positive"
+    elif number == 0:
+        sign = "zero"
+    else:
+        sign = "negative"
+
+    print(f"{number}: is {sign}")
+
+if __name__ == "__main__":
+    for _ in range(10):  # Replace 10 with the desired number of random numbers you want to generate.
+        random_sign()
+
+
