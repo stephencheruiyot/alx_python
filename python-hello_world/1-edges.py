26829100b5e9f247d2306b54e122bdb2c0348164
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-18 06:11:52 +0300

    add i-edges.py

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+word = "Holberton"
+
+#the first 3 letters "Hol"
+
+word_first_3 = word[:3]
+
+#the last two letters "on"
+word_last_2 = word[7:]
+
+#the middle word "olberto"
+middle_word = word[1:-1]
+
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
