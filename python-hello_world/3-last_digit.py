4c7c96fa11a7ab35b9cb16ce534e99734ff1d76e
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-18 07:17:01 +0300

    add "the last digit" file

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+result_string = "Last digit of {} is {} ".format(number, last_digit)
+
+if last_digit > 5:
+    result_string += "and is greater than 5"
+elif last_digit == 0:
+    result_string += "and is 0"
+else:
+    result_string += "and is less than 6 and not 0"
+
+print(result_string)
+
+ 
