11d4fafbce28357b2ddb8a333c73a56fc15b3361
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-19 19:46:16 +0300

    change code

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,6 @@
 for i in range(10):
     for j in range(i+1, 10):
-        print("{:02d}, ".format(i), end="")
-        print("{:02d}, ".format(j), end="")
-print()
+        print("{:d}{:d}, ".format(i, j), end="")
+
+print("\b\b\n" )
+
