21ab45fb8f874f1fb039274a648ab0851f1060eb
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-19 17:55:56 +0300

    remove abs(absolute)

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,7 +3,7 @@ import random
 
 number = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10
+last_digit = (number) % 10
 result_string = "Last digit of {} is {} ".format(number, last_digit)
 
 if last_digit > 5:
