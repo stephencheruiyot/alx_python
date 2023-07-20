b10c8dad97986ba8aed0ae1b70a450d8a47513bb
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-20 15:50:52 +0300

    add end tag

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,18 +3,19 @@ import random
 
 number = random.randint(-10000, 10000)
 
-last_digit = (number) % 10
-result_string = "Last digit of {} is {} ".format(number, last_digit)
+# Get the last digit of the number using the modulo operator (%)
+last_digit = abs(number) % 10
+
+# Print the output based on the last digit
+print("Last digit of", number, "is", last_digit, "and is", end=" ")
 
 if last_digit > 5:
-    result_string += "and is greater than 5"
+    print("greater than 5")
 elif last_digit == 0:
-    result_string += "and is 0"
+    print("0")
 else:
-    result_string += "and is less than 6 and not 0"
+    print("less than 6 and not 0")
 
-print(result_string)
-print()
 
 
  
