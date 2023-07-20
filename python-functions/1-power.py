30fadfe3c7551e103027504bc17e300d514bc628
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-20 23:39:24 +0300

    simplify code

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,7 +1,12 @@
 def pow(a, b):
-    if b == 0:
-        return 1
-    elif b < 0:
-        return 1 / pow(a, -b)
+    result = 1
+
+    if b >= 0:
+        for _ in range(b):
+            result *= a
     else:
-        return a * pow(a, b - 1)
+        for _ in range(abs(b)):
+            result /= a
+
+    return result
+
