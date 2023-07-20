b1e2fb6f149968e3ba971d410801fc26846c4ad4
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-20 23:21:49 +0300

    change code

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,19 +1,7 @@
-#Write a function that computes a to the power of b and return the value.
 def pow(a, b):
-    result = 1
-
-    # For positive or zero exponents
-    if b >= 0:
-        for _ in range(b):
-            result *= a
-
-    # For negative exponents
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / pow(a, -b)
     else:
-        for _ in range(-b):
-            result /= a
-
-    return result
-
-
-
-
+        return a * pow(a, b - 1)
