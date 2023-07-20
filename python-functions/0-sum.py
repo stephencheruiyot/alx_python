bafbc7c233de69f2159ab18a9a421970ed450395
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-20 23:35:58 +0300

    simplify code

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,8 +1,11 @@
 def add(a, b):
+    # Use the bitwise operations to simulate addition
     while b != 0:
-        carry = a & b
-        a = a ^ b
-        b = carry << 1
+        carry = a & b  # calculate the common set bits (carry bits)
+        a = a ^ b      # add bits of a and b without considering the carry
+        b = carry << 1 # shift carry by one position to the left
+
     return a
+
 
 
