2324e9cd9750e7664a01078bb0a665a4992dee20
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-20 22:31:33 +0300

    change code

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-# Get the last digit of the number using the modulo operator (%)
+
+# Extract the last digit of the number
 last_digit = abs(number) % 10
-# Print the output based on the last digit including a sign 
-sign = "-" if number < 0 else ""
-print(f"Last digit of {number} is {sign}{last_digit} and is")
+if number < 0:
+    last_digit *= -1
+
+# Determine the message based on the last digit
+message = "Last digit of {} is {} and is".format(number, last_digit)
+
 if last_digit > 5:
-    print("greater than 5")
+    message += " greater than 5"
 elif last_digit == 0:
-    print("0")
+    message += " 0"
 else:
-    print("less than 6 and not 0")
+    message += " less than 6 and not 0"
+
+# Print the message
+print(message)
+
 
 
 
