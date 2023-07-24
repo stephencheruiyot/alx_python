20f743c1ef31358633319455a43f5855a212f71a
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-24 07:34:52 +0300

    change format of files

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,10 @@
-a = 1
-b = 2
-
+# main_program.py
 if __name__ == "__main__":
+    a = 1
+    b = 2
+
     from add_0 import add
 
     result = add(a, b)
+
     print("{} + {} = {}".format(a, b, result))
