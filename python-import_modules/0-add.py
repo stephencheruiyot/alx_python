467b56e9506b9c9ce2a9c647e88052c71100b8a8
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-24 07:24:52 +0300

    modify .format

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     from add_0 import add
 
     result = add(a, b)
-    print("{a} + {b} = {result}".format(a, b, a + b))
+    print("{a} + {b} = {}".format(a, b, result))
