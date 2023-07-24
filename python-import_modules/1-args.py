f066e8fab465fb18dee4704ea463891489361d47
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-24 11:37:48 +0300

    remove space

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,9 +4,9 @@ def main():
     argv = sys.argv[1:]  # Exclude the script name from the arguments
     num_arguments = len(argv)
 
+
     if num_arguments == 0:
         print("0 argument.")
-        
     else:
         print(f"{num_arguments} {'argument' if num_arguments == 1 else 'arguments'}:")
 
