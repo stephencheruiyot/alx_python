f9d98f200d76edad3dbd58227ef87ef91d681318
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-24 11:19:23 +0300

    add pep 8 validation

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,18 +1,14 @@
 import sys
 
 def main():
-    argv = sys.argv[1:]
-    num_args = len(argv)
+    argv_length = len(sys.argv) - 1
+    arguments = sys.argv[1:]
 
-    print(f"Number of argument(s): {num_args}", end=' ')
-    if num_args == 0:
-        print(".",  end='\n')
-    elif num_args == 1:
-        print("argument:", end='\n')
-        print(f"1: {argv[0]}")
-    else:
-        print("arguments:", end='\n')
-        for i, arg in enumerate(argv, start=1):
+    print(f"Number of argument{'s' if argv_length != 1 else ''}: {argv_length}", end='')
+    print(f":{'.' if argv_length == 0 else ''}")
+
+    if argv_length > 0:
+        for i, arg in enumerate(arguments, start=1):
             print(f"{i}: {arg}")
 
 if __name__ == "__main__":
