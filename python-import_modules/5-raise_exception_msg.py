860040d3436e9ca663551e8bc0a38dc50c001372
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-25 17:05:38 +0300

    change code add pass

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,5 +1,5 @@
 def raise_exception_msg(message=""):
-    try:
-        raise NameError(message)
-    except NameError as e:
-        print("Name exception raised:", str(e))
+    class NameException(Exception):
+        pass
+    raise NameException(message)
+#
