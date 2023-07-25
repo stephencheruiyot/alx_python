f9276fe9923e49dcc0687671e9d8e47fefeec00a
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-25 21:59:06 +0300

    modify

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,14 +1,11 @@
-def main():
-    # Assign values to variables a and b
-    a = 1
-    b = 2
 
-    # Import the add function from add_0.py
-    import add_0
-    result = add_0.add(a, b)
+# Function to add two numbers
+def add(a, b):
+    return a + b
 
-    # Print the result
-    print("{} + {} = {}".format(a, b, result))
+# Define variables a and b
+a = 1
+b = 2
 
-if __name__ == "__main__":
-    main()
+# Print the result of the addition
+print("{a_value} + {b_value} = {result}".format(a_value=a, b_value=b, result=add(a, b)))
