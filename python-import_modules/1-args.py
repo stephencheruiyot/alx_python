cd4c7119fdec5b33adea0e5795aaafe836bc5b99
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-25 15:19:40 +0300

    make a script dynamic

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,16 @@
 import sys
 
-def main():
-    argv = sys.argv[1:]  # Exclude the script name from the arguments
-    num_arguments = len(argv)
+def print_arguments():
+    arguments = sys.argv[1:]  # Exclude the script name itself from the arguments list
+    num_arguments = len(arguments)
+
     if num_arguments == 0:
-        print("0 argument.")
+        print("0 arguments.")
     else:
         print(f"{num_arguments} {'argument' if num_arguments == 1 else 'arguments'}:")
-
-        for i, arg in enumerate(argv, 1):
+        for i, arg in enumerate(arguments, 1):
             print(f"{i}: {arg}")
+
 if __name__ == "__main__":
-    main()
+    print_arguments()
+
