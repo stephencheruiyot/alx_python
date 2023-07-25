4ddcd1662316dd4079a78a40889f233d36b36131
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-25 21:48:23 +0300

    modify 0-add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,14 @@
-#!/usr/bin/python3
-# main_program.py
-from add_0 import add
-a = 1
-b = 2
- 
-print("{} + {} = {}".format(a, b, add(a, b)))
+def main():
+    # Assign values to variables a and b
+    a = 1
+    b = 2
 
+    # Import the add function from add_0.py
+    import add_0
+    result = add_0.add(a, b)
+
+    # Print the result
+    print("{} + {} = {}".format(a, b, result))
+
+if __name__ == "__main__":
+    main()
