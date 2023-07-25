87ce7aee2b1f0ba0953b656b1a3cff8a37291ee3
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-25 15:51:06 +0300

    add Exception has been raised

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,7 +1,7 @@
 def raise_exception():
     try:
         # Attempt to raise a type exception
-        raise TypeError("This is a type exception.")
+        raise TypeError("Exception has been raised.")
     except TypeError as e:
         # Catch the raised exception and print the error message
         print("Exception raised:", e)
