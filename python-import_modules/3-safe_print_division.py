58a8eea0b59200ae2e6dc0dfd85bae359aff467f
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-25 15:38:47 +0300

    add none

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,7 +2,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+        print("Error: None.")
         return None
     except TypeError:
         print("Error: Both inputs must be integers.")
