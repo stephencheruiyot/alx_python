3d0fcff998f5efc4ee29a4baad816711e9a5cb0b
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-25 16:02:16 +0300

    add exception raised

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,7 +1,5 @@
 def raise_exception():
-    try:
-        # Attempt to raise a type exception
-        raise TypeError("Exception has been raised.")
-    except TypeError as e:
-        # Catch the raised exception and print the error message
-        print("Exception raised:", e)
+    x = "This is a string, not a number"
+    if not isinstance(x, int):
+        raise TypeError("Expected an integer, but got a string.")
+print("exception raised")
