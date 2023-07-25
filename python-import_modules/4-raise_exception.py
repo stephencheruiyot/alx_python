3bd57f0b00d5a023d532fcc788193151f6d56862
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-25 16:03:55 +0300

    delete print

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -2,4 +2,3 @@ def raise_exception():
     x = "This is a string, not a number"
     if not isinstance(x, int):
         raise TypeError("Expected an integer, but got a string.")
-print("exception raised")
