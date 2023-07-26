ebfe25fb2167ea5c927a0768227da625394c5db3
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-26 08:23:29 +0300

    add common elements

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,10 @@
+def common_elements(set_1, set_2):
+    #define two sets
+    set_1 = {}
+    set_2 = {}
+    #find the intersection of set_1 and set_2
+    common_elements = set_1.intersection(set_2)
+    
+print(common_elements)
+
+    
