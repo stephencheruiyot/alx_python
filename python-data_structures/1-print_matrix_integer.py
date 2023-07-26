f1ac4a24f42678deaa43dc1a46b224e3e7ccdb40
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-26 15:38:59 +0300

    pep 8 valid

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,12 +3,3 @@ def print_matrix_integer(matrix=[[]]):
         for num in row:
             print("{:d}".format(num), end=" ")
         print()
-
-# Example usage:
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
