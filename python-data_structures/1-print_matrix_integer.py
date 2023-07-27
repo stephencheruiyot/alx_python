927ee062390047164e80b9382313bba186455ab2
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-27 13:10:51 +0300

    new

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,11 @@
 def print_matrix_integer(matrix=[[]]):
+    # Get the number of rows and columns in the matrix
     rows = len(matrix)
-    cols = len(matrix[0]) if rows > 0 else 0
+    cols = len(matrix[0])
+
+    # Iterate through the rows and columns and print the matrix
     for i in range(rows):
         for j in range(cols):
-          print("{:d}".format(matrix[i][j]), end=' ')
-        print()  
+            print("{:d}".format(matrix[i][j]), end=" " if j < cols - 1 else "")
+        print()
+
