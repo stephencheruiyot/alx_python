b313a2ad6e563463843552b6d034efee159e938d
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-27 12:51:19 +0300

    reduce space

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=" ")
+            print("{:d}".format(num), end="")
         print() 
