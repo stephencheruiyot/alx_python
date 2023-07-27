d3ab6c8137c571d2530d6caf4c8cb565dab3931d
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-27 09:14:56 +0300

    change.format

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -11,8 +11,8 @@ def safe_print_division(a, b):
         print("An unexpected error occurred:", e)
         return None
     finally:
-        print("Inside result: {}".format(result))
+        print("Inside result: {}".format(a/b))
 
-        return result
+    return result
 
 
