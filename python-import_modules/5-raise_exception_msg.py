740d261821513d03b6aa0ada13a22e087154582c
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-07-27 12:37:33 +0300

    raise

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,8 +1,2 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
-
-# Test the function
-try:
-    raise_exception_msg("This is a custom name exception!")
-except NameError as e:
-    print("Caught the exception:", e)
