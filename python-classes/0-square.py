9a58f66b6e8d5ec4711a50451472aafb24734b50
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-01 21:40:14 +0300

    add docstrings

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,16 @@
-class square:
-    def __init__(self):
-            self.__size = 0
+class Square:
+    """A class that defines a square by its size.
+    
+    
+   Attributes: 
+    -__size (int) : The size of the square(side lenght).
+    """
+    def __init__(self, size):
+        """
+        Initializes an instance of `Square` with given size.
+
+        Args:
+            size (int):The size of the square (side length). 
+        """
+        self.__size = size
             
-size = square()
