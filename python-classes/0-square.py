3c08618cae94d403beb7e8b20f1c96be4ee5d3ca
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-01 22:05:43 +0300

    add docstring

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,19 +1,19 @@
 class Square:
     """
-    A class that defines a square by its size.
+    This class represents a square defined by its size.
 
     Attributes:
-        size (int): The size of the square (side length).
+        size (int): The size of the square.
     """
 
     def __init__(self, size):
         """
-        Initializes a Square instance with the given size.
+        Constructor for Square class.
 
         Args:
-            size (int): The size of the square (side length).
+            size (int): The size of the square.
         """
-        self._size = size
+        self.size = size
 
     def area(self):
         """
@@ -22,31 +22,4 @@ class Square:
         Returns:
             int: The area of the square.
         """
-        return self._size ** 2
-
-    def perimeter(self):
-        """
-        Calculate the perimeter of the square.
-
-        Returns:
-            int: The perimeter of the square.
-        """
-        return 4 * self._size
-
-    def get_size(self):
-        """
-        Get the size of the square.
-
-        Returns:
-            int: The size of the square (side length).
-        """
-        return self._size
-
-    def set_size(self, new_size):
-        """
-        Set the size of the square to a new value.
-
-        Args:
-            new_size (int): The new size of the square (side length).
-        """
-        self._size = new_size
+        return self.size ** 2
