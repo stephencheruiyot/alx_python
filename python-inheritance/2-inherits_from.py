c61b03f1ef0bbc5038e77d341cb13f5fc701b238
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-03 15:24:45 +0300

    add blank lines

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,7 +1,9 @@
 """
 A defining function.
 """
+
 def inherits_from(obj, a_class):
+    
     """
     Check if the object is an instance of a class
     that inherited from the given class.
@@ -14,8 +16,12 @@ def inherits_from(obj, a_class):
         bool: True if the object is an instance of 
         a class inherited from 'a_class', otherwise False.
     """
+    
     classes = type(obj).mro()
 
     # Check if 'a_class' is in the set of classes the object belongs to
-    return any(a_class is c for c in classes[1:])  
+    
+    return any(a_class is c for c in classes[1:]) 
+ 
 # Start from index 1 to skip the object's own class
+
