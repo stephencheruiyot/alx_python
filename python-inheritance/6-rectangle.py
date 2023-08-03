26f2327bc178ffee129ad992ebac053993bd1884
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-03 21:40:29 +0300

    remove .

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -71,6 +71,6 @@ class Rectangle(BaseGeometry):
         ValueError: If the value is not a positive integer.
         """
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer.".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
