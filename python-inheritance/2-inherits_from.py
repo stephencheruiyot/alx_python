1f321858c0cf2ac1e3a08952966a2a1a0b26b2f5
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-03 15:14:20 +0300

    PEP8 2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,24 +1,16 @@
-"""
-   A function with two parameters
-    
-"""
 def inherits_from(obj, a_class):
     """
-    Check if the object is an instance of a class 
-    that inherited
-    from the given class.
+    Check if the object is an instance of a class
+    that inherited from the given class.
 
     Args:
-        obj: The object to be checked.
-        a_class: The specified class to check inheritance from.
+        obj (object): The object to be checked.
+        a_class (class): The specified class to check inheritance from.
 
     Returns:
-        True if the object is an instance of a class inherited from 'a_class', otherwise False.
+        bool: True if the object is an instance of a class inherited from 'a_class', otherwise False.
     """
-        
     classes = type(obj).mro()
 
     # Check if 'a_class' is in the set of classes the object belongs to
-    return any(a_class is c for c in classes[1:])  
-# Start from index 1 to skip the object's own class
-
+    return any(a_class is c for c in classes[1:])  # Start from index 1 to skip the object's own class
