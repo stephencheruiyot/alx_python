5df989e43a335b07048e9ad608b013fe7008e1b7
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-03 06:31:13 +0300

    add Integer validator

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,46 @@
+"""
+    
+A class BaseGeometry
+"""
+
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+
+    Methods
+    -------
+    area()
+        Raises an Exception with the message "area() is not implemented."
+    integer_validator(name, value)
+        Validates the input value as an integer, raising appropriate exceptions if not valid.
+    """
+
+    def area(self):
+        """
+        Raises an Exception to indicate that the area() method is not implemented in the subclass.
+        """
+        raise Exception("area() is not implemented.")
+
+    def integer_validator(self, name, value):
+        """
+        Validates the input value as an integer.
+
+        Parameters
+        ----------
+        name : str
+            The name of the value being validated.
+        value : int
+            The value to be validated.
+
+        Raises
+        ------
+        TypeError
+            If the value is not an integer.
+        ValueError
+            If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
