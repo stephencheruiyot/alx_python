f204d7bdd429ce3235234deb27431be2c980616d
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-03 07:09:51 +0300

    fix comments

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -10,12 +10,12 @@ class BaseGeometry:
         """
         raise NotImplementedError("Subclasses must implement this method.")
 
-        """
+"""
         A class Rectangle
         argument:BaseGeometry
 
         
-        """
+"""
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """
@@ -32,12 +32,12 @@ class Rectangle(BaseGeometry):
         :return: Area of the rectangle
         """
         return self.width * self.height
-        """
+"""
        A class Square
        Arguments:Rectangle
 
         
-        """
+ """
 
 class Square(Rectangle):
     def __init__(self, size):
