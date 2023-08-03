3826947bf6276ad0cd64e664ee4900b2b10583dd
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-03 15:30:16 +0300

    delete spaces

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,27 +1,18 @@
 """
 A defining function.
 """
-
 def inherits_from(obj, a_class):
-    
     """
     Check if the object is an instance of a class
     that inherited from the given class.
-
     Args:
         obj (object): The object to be checked.
         a_class (class): The specified class to check inheritance from.
-
     Returns:
         bool: True if the object is an instance of 
         a class inherited from 'a_class', otherwise False.
     """
-    
     classes = type(obj).mro()
-
     # Check if 'a_class' is in the set of classes the object belongs to
-    
     return any(a_class is c for c in classes[1:]) 
- 
-# Start from index 1 to skip the object's own class
-
+     # Start from index 1 to skip the object's own class
