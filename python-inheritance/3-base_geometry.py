297a461c6137e4058b63b27ecf3f421af708dcdd
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-03 06:12:30 +0300

    Add geometry module

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,17 @@
+"""
+  An empty class with no attributes 
+  nor methods  
+"""
+
+class BaseGeometry:
+    """An empty class representing a base geometry.
+
+    This class will serve as the base for other geometry-related classes.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
