17b4014c793eba941670cf3cf276ad2094c3e5e0
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-06 00:06:34 +0300

    overide metaclass

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,8 +2,12 @@
   An empty class with no attributes 
   nor methods  
 """
+class BaseGeometryMetaclass(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+        
 
-class BaseGeometry():
+class BaseGeometry(metaclass=BaseGeometryMetaclass):
     
     """
     def a function __dir__ to get access to the super attributes
