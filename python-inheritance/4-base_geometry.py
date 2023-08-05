a03b9b5d0cc76db40fc1bedd5a54a97963f6753c
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-06 00:12:27 +0300

    remov e subclass

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -5,9 +5,14 @@ Raises:
         Exception:  area() is not implemented
 """
 
+from collections.abc import Iterable
+
+
 class BaseGeometry:
     """Base class for geometrical shapes."""
-
+    def __dir__(cls):
+        return [ attribute for attribute in super().__dir__() != '__init_subclass__']
+        
     def area(self):
         """Calculate the area of the geometrical shape.
         
