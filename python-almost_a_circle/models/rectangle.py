3bb025d61b2136beaea2d178e1e2d022d475aee7
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-08 16:18:32 +0300

    use type inplace of isinstance

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -47,9 +47,9 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setter for width attribute"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if int <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -61,9 +61,9 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """setter for the height"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if int <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -75,7 +75,9 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """setter for the x variable"""
-        if int < 0:
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -87,6 +89,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """setter for the y co-ordinate"""
-        if int < 0:
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
