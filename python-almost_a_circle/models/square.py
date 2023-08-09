c2cf6ad7de0c11a4787e20727574aca2f0decc68
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-09 07:47:08 +0300

    documentation added

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,4 +1,6 @@
-# square.py - contains the class Square that inherits from Rectangle
+"""
+square.py - contains the class Square that inherits from Rectangle class
+"""
 
 from models.rectangle import Rectangle
 
@@ -10,27 +12,46 @@ class Square(Rectangle):
         Initialize the Square object with provided size and position.
 
     Arguments:
-    size (int): The size of the square (both width and height).
+    size (int): The size of the square's sides.
     x (int, optional): The x-coordinate of the square's position. Defaults to 0.
     y (int, optional): The y-coordinate of the square's position. Defaults to 0.
     id (int, optional): The ID of the square. If not provided, it will be set by the Base class.
     """
-
+    
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize the Square object with provided size and position."""
-        super().__init__(size,size, x, y, id)  # Call superclass constructor with size for both width and height
+        super().__init__(size, size, x, y, id)  # Call superclass constructor with width and height as size
 
     @property
     def size(self):
-        """int: Getter for the size of the square."""
-        return self.width  # Size is the same as width for a square
+        """int: Getter for the size of the square's sides."""
+        return self.width  # Size of square is same as width and height
 
     @size.setter
     def size(self, value):
-        """setter for size attribute"""
+        """Setter for the size of the square's sides."""
         self.width = value
-        self.height = value  # Update both width and height with the same value
+        self.height = value
 
     def __str__(self):
         """Return a string representation of the square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        '''Update the values of the Square class
+        
+        Args:
+             *args: Variable number of arguments for updating the attributes in the order:
+                    id, size, x, y.
+        kwargs: Keyword arguments for updating the attributes. Allowed keys are 'id', 'size', 'x', and 'y'.
+        '''
+        attrs = ['id', 'size', 'x', 'y']
+        # Update using positional arguments
+        for idx, arg in enumerate(args):
+            setattr(self, attrs[idx], arg)
+        
+        # Update using keyword arguments
+        for key, value in kwargs.items():
+            if key in attrs:
+                setattr(self, key, value)
