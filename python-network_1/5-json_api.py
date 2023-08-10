122de565cce10f0a0be01ed4b5c087e85de25d06
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-10 14:08:35 +0300

    add comments

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,24 +1,45 @@
+#!/usr/bin/python3
+"""
+This script takes a letter as input and sends a POST request to http://0.0.0.0:5000/search_user.
+The letter is sent as the parameter 'q'.
+If no argument is given, 'q' is set to an empty string.
+If the response body is properly JSON formatted and not empty, it displays the id and name in the format: [<id>] <name>.
+Otherwise, it displays appropriate error messages.
+Only the 'requests' and 'sys' packages are used.
+"""
+
 import requests
 import sys
 
 def search_user(letter):
+    """
+    Sends a POST request to the specified URL with the provided letter as the 'q' parameter.
+    Prints the user information if available, or appropriate error messages.
+    """
     url = "http://0.0.0.0:5000/search_user"
-    params = {'q': letter}
-    response = requests.post(url, data=params)
+    data = {'q': letter}
 
     try:
-        json_data = response.json()
-        if json_data:
-            print("[{}] {}".format(json_data['id'], json_data['name']))
+        response = requests.post(url, data=data)
+        response_json = response.json()
+
+        if response_json:
+            user_id = response_json.get('id')
+            user_name = response_json.get('name')
+            if user_id is not None and user_name is not None:
+                print("[{}] {}".format(user_id, user_name))
+            else:
+                print("No result")
         else:
             print("No result")
+
+    except requests.exceptions.RequestException as e:
+        print(e)
     except ValueError:
         print("Not a valid JSON")
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        letter = ""
-    else:
-        letter = sys.argv[1]
-
-    search_user(letter)
+        search_user("")
+    elif len(sys.argv) == 2:
+        search_user(sys.argv[1])
