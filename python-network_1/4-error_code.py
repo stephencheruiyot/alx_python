8c0282ebd22a1c49caaebc6415cc898f70369273
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-10 12:18:14 +0300

    add module comment

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,6 +1,15 @@
 import requests
 import sys
 
+"""
+    Main function that takes a URL as input, sends a request to the URL,
+    and displays the body of the response. If the HTTP status code is
+    greater than or equal to 400, it prints an error message with the
+    status code.
+
+    Usage: python 4-error_code.py <URL>
+"""
+
 def main():
     """
     Main function that takes a URL as input, sends a request to the URL,
