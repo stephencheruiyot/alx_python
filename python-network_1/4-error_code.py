24fa1f614ee32d7d37f819e468e4aca1e37eec6a
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-10 20:55:39 +0300

    correction of 4 error code

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,33 +1,38 @@
-#!/usr/bin/python3
-""" HTTP Request Script
+#!/usr/bin/env python3
+
+""" This script takes a URL as input, sends a request to the URL,
+    and displays the response body. If the HTTP status code is
+    greater than or equal to 400, it prints an error message
+    with the HTTP status code.
 """
 
 import requests
 import sys
 
-def make_request(url):
-    """ Sends a request to the given URL and displays the response body.
-
-    Args:
-        url (str): The URL to send the request to.
-
-    Returns:
-        None
+def main():
+    """ Main function to execute the script
     """
-    try:
-        response = requests.get(url)
-        print(response.status_code)
-
-        if response.status_code >= 400:
-            print(f"Error code: {response.status_code}")
-    finally: 
-        pass
-    
-
-if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
     if len(sys.argv) != 2:
-        print("Usage: python script_name.py <URL>")
+        print("Usage: python3 script.py <URL>")
         sys.exit(1)
 
     url = sys.argv[1]
-    make_request(url)
+
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url)
+
+        # Print the response body
+        print(response.text)
+
+        # Check for HTTP status code >= 400
+        if response.status_code >= 400:
+            print(f"Error code: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
