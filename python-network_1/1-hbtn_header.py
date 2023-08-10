002124457fbeb73efe994f11c2406fa256dae0f9
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-10 19:46:04 +0300

    add 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,42 +1,30 @@
+#!/usr/bin/python3
 """
-   A module that Sends a request to the given URL and displays the value of the X-Request-Id variable in the response header.
+Sends a request to a URL and displays the value of the variable X-Request-Id in the response header.
+Uses the requests and sys packages only.
+"""
 
-    Args:
-        url (str): The URL to send the request to.
-
-    Returns:
-        str: The value of the X-Request-Id variable in the response header, or an error message.
-    """
-
-import requests
 import sys
+import requests
 
-def get_request_id(url):
-    """
-    Send a request to the given URL and display the value of the X-Request-Id header.
-
-    Args:
-        url (str): The URL to send the request to.
-
-    Returns:
-        None
-    """
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    
     try:
         response = requests.get(url)
         x_request_id = response.headers.get('X-Request-Id')
         
         if x_request_id:
-            print("X-Request-Id:", x_request_id)
+            print(x_request_id)
         else:
-            print("X-Request-Id not found in response headers.")
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
+            print("X-Request-Id header not found in the response.")
+            
+    except requests.RequestException as e:
+        print("An error occurred: {}".format(e))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <URL>")
-        sys.exit(1)
-    
-    url = sys.argv[1]
-    get_request_id(url)
-
+    main()
