656e01fb1d1908c354140bb5a467c6f5384dd2f1
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-10 11:44:30 +0300

    code modify

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -8,35 +8,35 @@
         str: The value of the X-Request-Id variable in the response header, or an error message.
     """
 
-import sys
 import requests
-"""
-    Sends a request to the given URL and displays the value of the X-Request-Id variable in the response header.
+import sys
+
+def get_request_id(url):
+    """
+    Send a request to the given URL and display the value of the X-Request-Id header.
 
     Args:
         url (str): The URL to send the request to.
 
     Returns:
-        str: The value of the X-Request-Id variable in the response header, or an error message.
-"""
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <url>")
-        return
-
-    url = sys.argv[1]
-    response = requests.get(url)
-    
-    if response.status_code == 200:
+        None
+    """
+    try:
+        response = requests.get(url)
         x_request_id = response.headers.get('X-Request-Id')
+        
         if x_request_id:
             print("X-Request-Id:", x_request_id)
         else:
-            print("X-Request-Id header not found in the response.")
-    else:
-        print("Request failed with status code:", response.status_code)
+            print("X-Request-Id not found in response headers.")
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
 
 if __name__ == "__main__":
-    main()    
-         
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <URL>")
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    get_request_id(url)
+
