36a1720c5b6c58a0f9f92ca99d0acebcd7d508c3
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-10 21:03:53 +0300

    correction

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -13,9 +13,7 @@ def main():
     """ Main function to execute the script
     """
     # Check if the correct number of arguments is provided
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <URL>")
-        sys.exit(1)
+   
 
     url = sys.argv[1]
 
@@ -32,7 +30,7 @@ def main():
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
-        sys.exit(1)
+    
 
 if __name__ == "__main__":
     main()
