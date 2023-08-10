2763412463f3597a4efb55e0d4fe19548ea0f8ac
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-10 12:22:33 +0300

    add 5-json_api.py

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+
+def search_user(letter):
+    url = "http://0.0.0.0:5000/search_user"
+    params = {'q': letter}
+    response = requests.post(url, data=params)
+
+    try:
+        json_data = response.json()
+        if json_data:
+            print("[{}] {}".format(json_data['id'], json_data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+
+    search_user(letter)
