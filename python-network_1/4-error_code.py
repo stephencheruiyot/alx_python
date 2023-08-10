eb29b89a478a6a467d6e001487fe69f10b64ad60
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-10 20:38:44 +0300

    correction1

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -20,8 +20,9 @@ def make_request(url):
 
         if response.status_code >= 400:
             print(f"Error code: {response.status_code}")
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
+    finally: 
+        pass
+    
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
