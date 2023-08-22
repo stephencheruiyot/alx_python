f67cb4ffc77232df6137ce8f16a00151c6d9c462
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-23 00:28:22 +0300

    add def and try block

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,35 +1,41 @@
-#!/usr/bin/python3
-"""
-Lists all states with a name starting with N from the database hbtn_0e_0_usa
-"""
-import sys
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    
+def list_states_with_n(username, password, database):
     # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        passwd="Folio9470m",
-        db="hbtn_0e_0_usa"
-    )
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+        return
 
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
+    # Create a cursor to execute SQL queries
+    cursor = conn.cursor()
 
-    # Execute the query to select states with names starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    # Execute the SQL query to fetch states starting with 'N'
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
     cursor.execute(query)
 
-    # Fetch all the rows from the result set
+    # Fetch and display the results
     results = cursor.fetchall()
-
-    # Print the results
     for row in results:
         print(row)
 
-    # Close the cursor and the database connection
+    # Close the cursor and the connection
     cursor.close()
-    db.close()
+    conn.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        list_states_with_n(username, password, database)
