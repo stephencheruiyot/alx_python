546b52e358c9aa3a1da6263cb78632954df960fc
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-23 01:05:40 +0300

    add import

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-"""Lists all State objects containing the letter 'a' from the database hbtn_0e_6_usa"""
+"""Lists all State objects that contain the letter a from the database hbtn_0e_6_usa."""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+def list_states_with_a(username, password, database):
+    # Create the MySQL connection
+    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}")
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query and display State objects containing the letter 'a'
+    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    for state in states_with_a:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
+    session.close()
+
 if __name__ == "__main__":
-    
-        username = "root"
-        password = "Folio9470m"
-        database ="hbtn_0e_6_us"
-        # Create an engine and bind it to the base
-        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}', pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-
-        # Create a session to interact with the database
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        # Retrieve State objects containing the letter 'a'
-        states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-
-        # Print the results
-        for state in states_with_a:
-            print(f"{state.id}: {state.name}")
-
-        # Close the session
-        session.close()
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        list_states_with_a(username, password, database)
