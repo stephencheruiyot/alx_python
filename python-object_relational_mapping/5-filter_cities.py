92b3062115b6cca6cfd75290c93c7bbfe8645253
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-23 00:48:15 +0300

    add cities by states

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,46 +1,47 @@
-
 import MySQLdb
+import sys
+
+def list_cities_by_state(username, password, database, state_name):
+    # Connect to the MySQL server
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database,
+            port=3306
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+        return
+
+    # Create a cursor to execute SQL queries
+    cursor = conn.cursor()
+
+    # Execute the SQL query to fetch cities by state name using parameterized query
+    query = "SELECT cities.id, cities.name, states.name \
+             FROM cities \
+             INNER JOIN states ON cities.state_id = states.id \
+             WHERE states.name = %s \
+             ORDER BY cities.id ASC"
+    
+    cursor.execute(query, (state_name,))
+
+    # Fetch and display the results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Close the cursor and the connection
+    cursor.close()
+    conn.close()
 
 if __name__ == "__main__":
-
- def filter_cities(username, password, database, state_name):
-    
-        # Connect to the MySQL server
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            passwd="Folio9470m",
-            db="hbtn_0e_4_usa"
-        )
-
-        # Create a cursor object
-        cursor = db.cursor()
-
-        # Define the prepared statement to retrieve cities of the specified state
-        cities_query = """
-        SELECT cities.name
-        FROM states
-        INNER JOIN cities ON states.id = cities.state_id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-        """
-
-        # Execute the prepared statement with the state name
-        cursor.execute(cities_query, (state_name,))
-
-        # Commit changes to the database
-        database.commit()
-
-        # Fetch all the rows from the result set
-        results = cursor.fetchall()
-
-        # Display the results
-        city_names = [row[0] for row in results]
-        print(", ".join(city_names))
-
-        # Close the cursor and the database connection
-        cursor.close()
-        db.close()
-        
-
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database> <state_name>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        state_name = sys.argv[4]
+        list_cities_by_state(username, password, database, state_name)
