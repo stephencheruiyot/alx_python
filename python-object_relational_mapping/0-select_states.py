e70035e22066c3c2732c5f566d715aa12d69ff6a
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-22 15:25:02 +0300

    Add file 0 states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,6 @@
-"""
-Lists all states from the database hbtn_0e_0_usa
-"""
-
 import MySQLdb
 
-
 if __name__ == "__main__":
-   
     # Connect to the MySQL server
     db = MySQLdb.connect(
         host="localhost",
@@ -19,9 +13,30 @@ if __name__ == "__main__":
     # Create a cursor object to interact with the database
     cursor = db.cursor()
 
+    # Create the "states" table if it doesn't exist
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS states (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(256) NOT NULL,
+        PRIMARY KEY (id)
+    )
+    """
+    cursor.execute(create_table_query)
+
+    # Insert data into the "states" table
+    insert_data_query = """
+    INSERT INTO states (name) VALUES
+        ('California'),
+        ('Arizona'),
+        ('Texas'),
+        ('New York'),
+        ('Nevada')
+    """
+    cursor.execute(insert_data_query)
+
     # Execute the query to select states in ascending order by states.id
-    query = "SELECT * FROM states ORDER BY id ASC"
-    cursor.execute(query)
+    select_query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(select_query)
 
     # Fetch all the rows from the result set
     results = cursor.fetchall()
@@ -33,4 +48,3 @@ if __name__ == "__main__":
     # Close the cursor and the database connection
     cursor.close()
     db.close()
-
