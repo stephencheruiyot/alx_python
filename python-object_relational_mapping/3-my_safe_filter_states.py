166d58cf3d54da4c149948a82a846346ae5e0a7c
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-22 08:11:26 +0300

    add file- my safe filter

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table of hbtn_0e_0_usa where name matches the argument
+Displays all values in the states table of hbtn_0e_0_usa where name matches the argument (safe from MySQL injection)
 """
 
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-   
+    
+
     # Connect to the MySQL server
     db = MySQLdb.connect(
         host="localhost",
@@ -14,15 +16,13 @@ if __name__ == "__main__":
         user="root",
         passwd="Folio9470m",
         db="hbtn_0e_0_usa"
-    
-    )
 
     # Create a cursor object to interact with the database
     cursor = db.cursor()
 
-    # Use format to create the SQL query with user input
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format("state_name")
-    cursor.execute(query)
+    # Use parameterized query to prevent SQL injection
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
 
     # Fetch all the rows from the result set
     results = cursor.fetchall()
