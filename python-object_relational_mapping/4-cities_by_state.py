b2bc6b2200f955a2244f266b644dd540b9465fed
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-23 01:18:51 +0300

    change query

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -18,8 +18,13 @@ def list_cities(username, password, database):
     # Create a cursor to execute SQL queries
     cursor = conn.cursor()
 
-    # Execute the SQL query to fetch cities
-    query = "SELECT * FROM cities ORDER BY cities.id ASC"
+    # Execute the SQL query to fetch cities with their corresponding states
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+    """
     cursor.execute(query)
 
     # Fetch and display the results
