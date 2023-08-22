6ad8d75e417e5214b2dd0c12d7c457d34af95e38
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-23 00:40:40 +0300

    add format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 import MySQLdb
 import sys
 
-def search_states_by_name(username, password, database, state_name):
+def search_states(username, password, database, state_name):
     # Connect to the MySQL server
     try:
         conn = MySQLdb.connect(
@@ -18,9 +18,9 @@ def search_states_by_name(username, password, database, state_name):
     # Create a cursor to execute SQL queries
     cursor = conn.cursor()
 
-    # Execute the SQL query to fetch states matching the provided name
-    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
-    cursor.execute(query, (state_name,))
+    # Execute the SQL query to fetch matching states
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state_name)
+    cursor.execute(query)
 
     # Fetch and display the results
     results = cursor.fetchall()
@@ -39,4 +39,4 @@ if __name__ == "__main__":
         password = sys.argv[2]
         database = sys.argv[3]
         state_name = sys.argv[4]
-        search_states_by_name(username, password, database, state_name)
+        search_states(username, password, database, state_name)
