91f89728f2415a3347a56d148aea64c8119a4ea7
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-22 17:34:34 +0300

    add model file

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Start link class to table in database 
+"""
+import sys
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Create an instance of the declarative base
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+
+        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}', pool_pre_ping=True)
+
+        # Create a session to interact with the database
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # Create the tables
+        Base.metadata.create_all(engine)
