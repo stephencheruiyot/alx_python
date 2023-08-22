26ce7bbe1602a9b83ab2618c09dfbb0ca6edaf18
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-23 01:30:48 +0300

    modify user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,42 +1,42 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
+
 import MySQLdb
 import sys
 
-def search_states(username, password, database, state_name):
+if __name__ == "__main__":
+    # Check if all 4 arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database_name state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Retrieve command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
     # Connect to the MySQL server
     try:
-        conn = MySQLdb.connect(
-            host="localhost",
-            user=username,
-            passwd=password,
-            db=database,
-            port=3306
-        )
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+        cursor = db.cursor()
+
+        # Create the SQL query using the user input
+        query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+        cursor.execute(query, (state_name,))
+
+        # Fetch and display the results
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+        # Close the database connection
+        cursor.close()
+        db.close()
+
     except MySQLdb.Error as e:
-        print("Error connecting to the database:", e)
-        return
-
-    # Create a cursor to execute SQL queries
-    cursor = conn.cursor()
-
-    # Execute the SQL query to fetch matching states
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state_name)
-    cursor.execute(query)
-
-    # Fetch and display the results
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-
-    # Close the cursor and the connection
-    cursor.close()
-    conn.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> <state_name>")
-    else:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        state_name = sys.argv[4]
-        search_states(username, password, database, state_name)
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
