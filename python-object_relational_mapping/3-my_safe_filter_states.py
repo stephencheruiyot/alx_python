142a9be4b30176d27b99e5554f968e90abc82122
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-24 22:37:35 +0300

    pep8

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -19,9 +19,9 @@ def search_states_safe(username, password, database, state_name):
     # Create a cursor to execute SQL queries
     cursor = conn.cursor()
 
-    # Execute the SQL query to fetch matching states 
+    # Execute the SQL query to fetch matching states
     query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
-            
+
     cursor.execute(query, (state_name,))
 
     # Fetch and display the results
@@ -32,6 +32,7 @@ def search_states_safe(username, password, database, state_name):
     # Close the cursor and the connection
     cursor.close()
     conn.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
