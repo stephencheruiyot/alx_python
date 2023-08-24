1a38f22af8664c0f716b3a07cf2106650506a65c
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-24 21:47:31 +0300

    pep 8

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,6 +7,7 @@ from the database hbtn_0e_0_usa.
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     # Check if all 3 arguments are provided
     if len(sys.argv) != 4:
@@ -20,7 +21,11 @@ if __name__ == "__main__":
 
     # Connect to the MySQL server
     try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+        db = MySQLdb.connect(host="localhost", 
+                             port=3306, 
+                             user=username, 
+                             passwd=password, 
+                             db=database_name)
         cursor = db.cursor()
 
         # Create the SQL query to fetch states starting with "N"
