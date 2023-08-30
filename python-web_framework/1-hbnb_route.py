a24e5e6f15f40acec4ac203c87f326949b3854b9
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-30 14:35:33 +0300

    add scrip1

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,24 @@
+"""Flask Web Application
+This is a simple Flask web application that
+displays "Hello HBNB!" when you access the root URL.
+"""
+from flask import Flask
+
+# Create a Flask web application
+app = Flask(__name__)
+
+# Route definition for the root URL ("/") with strict_slashes set to False
+@app.route('/', '/hbnb' strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+# Route definition for "/hbnb"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+# Main entry point of the script
+if __name__ == '__main__':
+    # Start the Flask web application on 0.0.0.0, port 5000
+    app.run(host='0.0.0.0', port=5000)
