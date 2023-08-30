d70ffbc8e38b6e5cbb4fd530e05c49476ce28f65
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-08-30 13:58:53 +0300

    add documentation

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
+# Create a Flask web application
 app = Flask(__name__)
 
-# Define the route for the home page
+# Route definition for the root URL ("/") with strict_slashes set to False
 @app.route('/', strict_slashes=False)
-def home():
+def hello_hbnb():
     return "Hello HBNB!"
 
 if __name__ == '__main__':
+    # Start the Flask web application on 0.0.0.0, port 5000
     app.run(host='0.0.0.0', port=5000)
