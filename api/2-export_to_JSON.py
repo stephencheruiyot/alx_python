8a9550ad9fb681bb9fea17fb589718169a64bef0
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-10-05 21:54:05 +0300

    add export json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,54 +1,41 @@
-"""
-Employee Progress Module
-
-This module provides functions to retrieve and analyze employee progress based on their
-TODO list from a JSON API. It fetches the employee's tasks, counts completed tasks, and
-exports completed tasks to a JSON file.
-
-Usage:
-    $ python script.py <employee_id>
-
-Author: stephen cheruiyot
-"""
-import json
 import requests
+import json
 import sys
 
-def get_employee_todo_progress(employee_id):
-    # Define the API endpoints
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+def get_employee_info(employee_id):
+    # Define API endpoints
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_endpoint = f"{base_url}/users/{employee_id}"
+    todos_endpoint = f"{base_url}/users/{employee_id}/todos"
 
-    try:
-        # Fetch employee TODO list
-        todo_response = requests.get(todo_url)
-        todo_data = todo_response.json()
+    # Get user details
+    user_response = requests.get(user_endpoint)
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
 
-        # Fetch employee details
-        user_response = requests.get(user_url)
-        user_data = user_response.json()
+    # Get user's todo list
+    todos_response = requests.get(todos_endpoint)
+    todos_data = todos_response.json()
 
-        # Count completed and total tasks
-        total_tasks = len(todo_data)
-        completed_tasks = sum(1 for task in todo_data if task['completed'])
+    # Count completed tasks and total tasks
+    completed_tasks = [todo for todo in todos_data if todo["completed"]]
+    total_tasks = len(todos_data)
 
-        # Display employee TODO list progress
-        print(f"Employee {user_data['name']} is done with tasks ({completed_tasks}/{total_tasks}):")
+    # Prepare the output format
+    output = f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):"
 
-        # Display completed tasks and record them in a list
-        completed_task_list = []
-        for task in todo_data:
-            if task['completed']:
-                print(f"\t{task['title']}")
-                completed_task_list.append({"task": task['title'], "completed": True, "username": user_data['username']})
+    # Print the completed tasks
+    for todo in completed_tasks:
+        task_title = todo.get("title")
+        output += f"\n\t{task_title}"
 
-        # Export completed tasks to a JSON file
-        with open(f"{employee_id}.json", "w") as json_file:
-            json.dump({user_data['id']: completed_task_list}, json_file, indent=4)
+    # Print the output
+    print(output)
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+    # Save the data to a JSON file
+    user_info = {"USER_ID": [{"task": todo["title"], "completed": todo["completed"], "username": user_data["username"]} for todo in todos_data]}
+    with open(f"{employee_id}.json", "w") as json_file:
+        json.dump(user_info, json_file, indent=4)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -56,4 +43,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    get_employee_info(employee_id)
