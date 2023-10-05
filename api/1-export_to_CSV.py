1bbea9e35990ce140466e8cc39401857ef9db483
stephencheruiyot <stephencheruiyot58@gmail.com>
2023-10-05 21:17:00 +0300

    correct 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,60 +1,63 @@
 import csv
-import os
 import requests
+import sys
 
+def fetch_employee_info(employee_id):
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-# Base URL for the REST API
-BASE_URL = "https://jsonplaceholder.typicode.com"
+    try:
+        user_response = requests.get(user_url)
+        user_response.raise_for_status()
+        user_data = user_response.json()
+        
+        todos_response = requests.get(todos_url)
+        todos_response.raise_for_status()
+        todos_data = todos_response.json()
+        
+        return user_data, todos_data
+    except requests.exceptions.HTTPError as e:
+        print(f"Error fetching data: {e}")
+        sys.exit(1)
 
-def get_employee_info(employee_id):
-    # Get employee details
-    user_url = f"{BASE_URL}/users/{employee_id}"
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
+def calculate_progress(user_data, todos_data):
+    name = user_data.get("name")
+    todos_done = sum(1 for todo in todos_data if todo.get("completed"))
+    todos_count = len(todos_data)
 
-    # Get TODO list for the employee
-    todos_url = f"{BASE_URL}/users/{employee_id}/todos"
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
+    print(f"Employee {name} is done with tasks({todos_done}/{todos_count}):")
+    
+    completed_tasks = [todo.get("title") for todo in todos_data if todo.get("completed")]
+    for task in completed_tasks:
+        print(f"\t {task}")
 
-    # Calculate the progress
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
+def export_to_csv(user_data, todos_data):
+    user_id = user_data.get("id")
+    username = user_data.get("username")
 
-    # Print employee progress in the specified format
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
-    for todo in todos_data:
-        if todo['completed']:
-            print("\t {}".format(todo['title']))
-
-    # Export data to CSV
-    export_to_csv(employee_id, employee_name, todos_data)
-
-def export_to_csv(employee_id, employee_name, todos_data):
-    filename = f"{employee_id}.csv"
+    filename = f"{user_id}.csv"
     with open(filename, mode='w', newline='') as csv_file:
         fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
-
+        
         for todo in todos_data:
             writer.writerow({
-                'USER_ID': employee_id,
-                'USERNAME': employee_name,
-                'TASK_COMPLETED_STATUS': todo['completed'],
-                'TASK_TITLE': todo['title']
+                'USER_ID': user_id,
+                'USERNAME': username,
+                'TASK_COMPLETED_STATUS': todo.get("completed"),
+                'TASK_TITLE': todo.get("title")
             })
 
     print(f"Data exported to {filename}")
 
 if __name__ == "__main__":
-    try:
-        employee_id = int(input("Enter an employee ID: "))
-        get_employee_info(employee_id)
-    except ValueError:
-        print("Please enter a valid integer for the employee ID.")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    user_data, todos_data = fetch_employee_info(employee_id)
+    
+    calculate_progress(user_data, todos_data)
+    export_to_csv(user_data, todos_data)
